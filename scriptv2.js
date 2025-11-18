e4730acf07ff447fab544844712388bb52b93df3
juanwinograd <juan.winograd@gmail.com>
2025-11-18 09:01:33 -0300

    noviembre

--- scriptv2.js
+++ scriptv2.js
@@ -1,5 +1,5 @@
-const MES_ACTUAL = "octubre 2025"; const A_COBRAR = "noviembre 2025";
-const ULTIMO_IPC = "octubre 2025";
+const MES_ACTUAL = "noviembre 2025"; const A_COBRAR = "diciembre 2025";
+const ULTIMO_IPC = "noviembre 2025";
 document.getElementById("mes-actual").innerHTML = "Sueldo de "+MES_ACTUAL.slice(0, -5)+" (a cobrar en "+A_COBRAR.slice(0, -5)+")";
 const DescuentoOS = 0.06, DescuentoJubilacion = 0.13, DescuentoFCompensador = 0.003, DescuentoCajaComp = 0.045;
 var Rem = 1 - (DescuentoOS + DescuentoJubilacion + DescuentoFCompensador);
