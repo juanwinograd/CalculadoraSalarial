5b30f676f819c4663cc5dd04446604c73062a1d1
juanwinograd <juan.winograd@gmail.com>
2026-09-10 08:40:08 -0300

    sept

--- scriptv2.js
+++ scriptv2.js
@@ -1,6 +1,6 @@
-const MES_ACTUAL = "agosto 2026";
-const A_COBRAR = "septiembre 2026";
-const ULTIMO_IPC = "julio 2026";
+const MES_ACTUAL = "septiembre 2026";
+const A_COBRAR = "octubre 2026";
+const ULTIMO_IPC = "agosto 2026";
 const JUNIO_DICIEMBRE = false;
 
 document.getElementById("mes-actual").innerHTML = "Sueldo de "+MES_ACTUAL.slice(0, -5)+" (a cobrar en "+A_COBRAR.slice(0, -5)+")";
